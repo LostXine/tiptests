edf3a0fefee198f89ec2bb5c328a950fe0d1fa50
李响 <lostxine@gmail.com>
2017-01-06 11:54:09 +0800

    增加了子类，虚函数相关测试

---
+++ stlTests/test_cpp_class.cpp
@@ -0,0 +1,40 @@
+#include "stlcontrol.h"
+
+#ifdef TEST_CPP_CLASS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Thing
+{
+public:
+	virtual void what_Am_I() { cout << "I am a Thing.\n"; }
+	virtual ~Thing(){ cout << "Thing destructor" << endl; }
+};
+class Animal : public Thing
+{
+public:
+	void what_Am_I() { cout << "I am an Animal.\n"; }
+	~Animal(){ cout << "Animal destructor" << endl; }
+};
+
+int main()
+{
+	Thing *t = new Thing;
+	Animal*x = new Animal;
+	Thing* array[2];
+	array[0] = t;
+	array[1] = x;
+	for (int i = 0; i < 2; i++)
+	{
+		array[i]->what_Am_I();	//通过指针调用虚函数
+	}
+	cout << "Delete thing t:" << endl;
+	delete array[0];
+	cout << "Delete Animal x:" << endl;
+	delete array[1];
+	cin.ignore();
+	return 0;
+}
+#endif
